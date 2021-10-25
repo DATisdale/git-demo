0f0da83b55c129a599f7387b1dfd33806f49a649
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2021-10-24 21:01:21 -0400

    Added 1st prompt

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let randomDestination =('Las Vegas')
 let randomRestuarant =('Big Brutus')
 let randomTransportation =('Bike')
 let randomEntertainment =('Juggling')
-
+let userInput=("")
 
 alert("Here are your choices!"+ '\n' + randomDestination +'\n' + randomRestuarant + '\n' + randomTransportation + '\n' + randomEntertainment)
+
+prompt("Are you satisfied with the choices? Yes or No?", userInput)
