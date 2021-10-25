790f62051e26b29c6e5feb3aeccfc789709d5649
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2021-10-24 22:26:16 -0400

    Added random destination

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 "use strict"
 
 let destinations = ['NewYork', 'Las Vegas', 'Charlotte', 'Virgina Beach', 'Nashville', 'Charleston', 'Woodbidge', 'Atlanta']
-let restuarants = ['Big Brutus', 'Steak & Shake', 'Olive Garden', 'Tacos & Brunch', 'KickBackJacks', 'Tuesdays', 'Burger Work', 'Munchies']
+let restaurants = ['Big Brutus', 'Steak & Shake', 'Olive Garden', 'Tacos & Brunch', 'KickBackJacks', 'Tuesdays', 'Burger Work', 'Munchies']
 let trasnportation = ['Trolley', 'Taxi', 'Uber', 'Rental Car', 'Yatch', 'Helicopter', 'Skateboard', 'Bike']
 let entertainment = [ 'Kriss Kross Concert', 'Juggling', 'Painting', 'Mini Golfing', 'Bowling', 'Zoo', 'A Play', 'A Musical']
 
-let randomDestination =('Las Vegas')
-let randomRestuarant =('Big Brutus')
+let randomDestination = anotherDestination(destinations)
+let randomRestaurant =('Big Brutus')
 let randomTransportation =('Bike')
 let randomEntertainment =('Juggling')
-let userInput=("")
+let userInput=("no")
+let numberPrompt = (1)
 
-alert("Here are your choices!"+ '\n' + randomDestination +'\n' + randomRestuarant + '\n' + randomTransportation + '\n' + randomEntertainment)
+while(userInput == 'No' || userInput == 'NO' || userInput == 'no') {
+
+
+
+alert("Here are your choices!"+ '\n' + randomDestination +'\n' + randomRestaurant + '\n' + randomTransportation + '\n' + randomEntertainment)
 
 userInput = prompt("Are you satisfied with the choices? Yes or No?")
-if(userInput == 'Yes' || userInput == 'YES' || userInput == 'yes'){
-    alert("Your final choices are!" + '\n' + randomDestination + '\n' + randomRestuarant + '\n' + randomTransportation + '\n' +randomEntertainment)
+if(userInput == 'Yes' || userInput == 'YES' || userInput == 'yes') {
+    alert("Your final choices are!" + '\n' + randomDestination + '\n' + randomRestaurant + '\n' + randomTransportation + '\n' +randomEntertainment)
+
+}
+else if (userInput == 'No' || userInput == 'NO'|| userInput == 'no') {
+numberPrompt = prompt("What option would you like to change? \n Press 1 for Destination \n Press 2 for Restaurant \n Press 3 for Transportation \n Press 4 for Entertainment")
+
+if(numberPrompt == 1)
+randomDestination = anotherDestination(destinations)
+}
+
+
+}
+
+function anotherDestination (destinations) {
+let getRandomNumber = Math.floor(Math.random() * 8)
+return destinations[getRandomNumber]
 }
