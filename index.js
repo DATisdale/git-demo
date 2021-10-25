671db0fae64aacb3a9a61c73683fd0baab362d42
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2021-10-24 20:50:21 -0400

    added alert for choices

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict"
 
+let destinations = ['NewYork', 'Las Vegas', 'Charlotte', 'Virgina Beach', 'Nashville', 'Charleston', 'Woodbidge', 'Atlanta']
+let restuarants = ['Big Brutus', 'Steak & Shake', 'Olive Garden', 'Tacos & Brunch', 'KickBackJacks', 'Tuesdays', 'Burger Work', 'Munchies']
+let trasnportation = ['Trolley', 'Taxi', 'Uber', 'Rental Car', 'Yatch', 'Helicopter', 'Skateboard', 'Bike']
+let entertainment = [ 'Kriss Kross Concert', 'Juggling', 'Painting', 'Mini Golfing', 'Bowling', 'Zoo', 'A Play', 'A Musical']
+
+let randomDestination =('Las Vegas')
+let randomRestuarant =('Big Brutus')
+let randomTransportation =('Bike')
+let randomEntertainment =('Juggling')
+
+
+alert("Here are your choices!"+ '\n' + randomDestination +'\n' + randomRestuarant + '\n' + randomTransportation + '\n' + randomEntertainment)
