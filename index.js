83d8521db9ec32bb0a908bb54b3809c093727444
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2021-10-25 11:50:36 -0400

    completed project

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 let destinations = ['NewYork', 'Las Vegas', 'Charlotte', 'Virgina Beach', 'Nashville', 'Charleston', 'Woodbidge', 'Atlanta']
 let restaurants = ['Big Brutus', 'Steak & Shake', 'Olive Garden', 'Tacos & Brunch', 'KickBackJacks', 'Tuesdays', 'Burger Work', 'Munchies']
-let trasnportation = ['Trolley', 'Taxi', 'Uber', 'Rental Car', 'Yatch', 'Helicopter', 'Skateboard', 'Bike']
+let transportation = ['Trolley', 'Taxi', 'Uber', 'Rental Car', 'Yatch', 'Helicopter', 'Skateboard', 'Bike']
 let entertainment = [ 'Kriss Kross Concert', 'Juggling', 'Painting', 'Mini Golfing', 'Bowling', 'Zoo', 'A Play', 'A Musical']
 
 let randomDestination = anotherDestination(destinations)
 let randomRestaurant = anotherRestaurant(restaurants)
-let randomTransportation =('Bike')
-let randomEntertainment =('Juggling')
+let randomTransportation =anotherTransportation(transportation)
+let randomEntertainment =anotherEntertainment(entertainment)
 let userInput=("no")
 let numberPrompt = (1)
 
@@ -30,12 +30,30 @@ if(numberPrompt == 1)
 randomDestination = anotherDestination(destinations)
 else if(numberPrompt==2)
 randomRestaurant = anotherRestaurant(restaurants)
-
-
+else if(numberPrompt ==3)
+randomTransportation = anotherTransportation(transportation)
+else if(numberPrompt ==4)
+randomEntertainment = anotherEntertainment(entertainment)
 }
 }
 
 function anotherDestination (destinations) {
 let getRandomNumber = Math.floor(Math.random() * 8)
 return destinations[getRandomNumber]
+
+
+}
+
+function anotherRestaurant (restaurants){
+    let getRandomNumber = Math.floor(Math.random() * 8)
+    return restaurants[getRandomNumber]
+}
+function anotherTransportation (transportation) {
+    let getRandomNumber = Math.floor(Math.random() * 8)
+    return transportation [getRandomNumber]
+
+}
+function anotherEntertainment (entertainment) {
+    let getRandomNumber = Math.floor(Math.random() * 8)
+    return entertainment [getRandomNumber]
 }
